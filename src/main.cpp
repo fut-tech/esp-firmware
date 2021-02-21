458f9c74b4260eca6598cb9f7fcf7c1d1fc2d37c
LittleBuster <DenisovS21@gmail.com>
2021-02-22 04:51:07 +0700

    Add IoT framewaork

---
+++ src/main.cpp
@@ -0,0 +1,29 @@
+#include <Arduino.h>
+#include "LittleFS.h"
+
+#include "WiFiManager.h"
+#include "webServer.h"
+#include "updater.h"
+#include "fetch.h"
+#include "configManager.h"
+#include "timeSync.h"
+
+void setup() 
+{
+    Serial.begin(115200);
+
+    LittleFS.begin();
+    GUI.begin();
+    configManager.begin();
+    WiFiManager.begin(configManager.data.projectName);
+    timeSync.begin();
+
+    Serial.println("Hello world");
+}
+
+void loop() 
+{
+    WiFiManager.loop();
+    updater.loop();
+    configManager.loop();
+}
